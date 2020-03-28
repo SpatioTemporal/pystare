f52419d3e9e08e4de4945771f3a7a360cfc4b847
Michael Rilee <mike@rilee.net>
2020-03-27 20:43:34 -0400

    Version bump.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ data_files = []
 
 setup(
     name='pystare',
-    version='0.1.1',
+    version='0.2.0',
     description="",
     cmdclass={'build_py': build_py},
     long_description=LONG_DESCRIPTION,         
