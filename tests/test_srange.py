3d4483bb42638561f978399a070ad69302654c76
niklas <griessbaum@gmail.com>
2021-02-26 11:59:59 -0700

    added srange collapse tests

--- tests/test_srange.py
+++ tests/test_srange.py
@@ -2,15 +2,16 @@ import numpy
 import pystare 
 import unittest
 
-from sids import sids
 
 class MainTest(unittest.TestCase):
     
     def test_make_srange1000(self):
+        from tests.sids import sids
         sids = sids[0:1000]
         srange = pystare.srange(sids)
         
-    def test_make_srange10000(self)
+    def test_make_srange10000(self):
+        from tests.sids import sids
         sids = sids[0:1000]
         srange = pystare.srange(sids)
         
@@ -22,6 +23,23 @@ class MainTest(unittest.TestCase):
                             0x300a31000000000a,
                             0x300a31800000000a])
         ivs = pystare.to_compressed_range(sids.flatten())
-        indices = numpy.zeros([srange.get_size_as_values()], dtype=numpy.int64)
-        srange.copy_values(indices)
-        self.assertEqual()
+        #indices = numpy.zeros([srange.get_size_as_values()], dtype=numpy.int64)
+        #srange.copy_values(indices)
+        with self.subTest():
+            self.assertEqual(ivs.size, 1)
+        with self.subTest():    
+            self.assertEqual(hex(ivs[0]), '0x300a300000000009')
+        
+    def test_collapse2(self):
+        # parent with same ancestor
+        sids = numpy.array([0x300a30000000000a,
+                            0x300a30800000000a,
+                            0x300a31000000000a,
+                            0x300a31800000000a])
+        ivs = pystare.to_compressed_range(sids.flatten())
+        #indices = numpy.zeros([srange.get_size_as_values()], dtype=numpy.int64)
+        #srange.copy_values(indices)
+        with self.subTest():
+            self.assertEqual(ivs.size, 1)
+        with self.subTest():    
+            self.assertEqual(hex(ivs[0]), '0x300a300000000009')
