ea44155185408ca3df2c2fe7c5f5c18f79e23040
niklas <griessbaum@gmail.com>
2021-03-03 16:10:26 -0700

    added tests for multires intersection

--- tests/test_srange.py
+++ tests/test_srange.py
@@ -43,3 +43,25 @@ class MainTest(unittest.TestCase):
             self.assertEqual(ivs.size, 1)
         with self.subTest():    
             self.assertEqual(hex(ivs[0]), '0x300a300000000009')
+            
+    def test_multires_intersect1(self):
+        # Earth are the 8 initial trixels
+        earth = numpy.array([0x0000000000000000, 0x0800000000000000, 0x1000000000000000, 
+                             0x1800000000000000, 0x2000000000000000, 0x2800000000000000,
+                             0x3000000000000000, 0x3800000000000000])
+        sids = numpy.array([0x300a30000000000a, 0x300a30800000000a, 0x300a31000000000a, 0x300a31800000000a])
+        indices = pystare.intersect(earth, sids, multiresolution=True)
+        with self.subTest():
+            self.assertEqual(ivs.size, 1)
+        with self.subTest():    
+            self.assertEqual(hex(ivs[0]), '0x300a300000000009')
+            
+    def test_multires_intersect2(self):
+        sids = numpy.array([0x300a30000000000a, 0x300a30800000000a, 0x300a31000000000a, 0x300a31800000000a])
+        parent = pystare.expand_intervals(sids, resolution=9)
+        indices = pystare.intersect(parent, sids)
+        with self.subTest():
+            self.assertEqual(ivs.size, 1)
+        with self.subTest():    
+            self.assertEqual(hex(ivs[0]), '0x300a300000000009')
+        
