a3339dd06f87d35f6a0bae1591502574cd1f884e
niklas <griessbaum@gmail.com>
2021-09-01 12:04:56 +0200

    prebuild

--- .github/workflows/prebuild.sh
+++ .github/workflows/prebuild.sh
@@ -1,12 +1,15 @@
 git clone https://github.com/SpatioTemporal/STARE
 cd STARE
-set -x
+set -x # Print commands and their arguments as they are executed.
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=~/stare ..
+#cmake -DCMAKE_INSTALL_PREFIX=~/stare ..
+cmake ../
 make
 make install
-STARE_LIB_DIR=~/stare/lib/
-STARE_INCLUDE_DIR=~/stare/include/
+
+# Set environment vars for setup.py
+#STARE_LIB_DIR=~/stare/lib/
+#STARE_INCLUDE_DIR=~/stare/include/
 #CPPFLAGS: "-I/usr/include -I/usr/local/include -I/home/runner/stare/include"
 #LDFLAGS: "-L/home/runner/stare/lib"
