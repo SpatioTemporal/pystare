3bca06ca687ff5ad9a8dfa8c5d0fa0669eea9b0f
Niklas Griessbaum <griessbaum@gmail.com>
2022-11-23 11:52:58 -0800

    pypi

--- .github/workflows/prebuild.sh
+++ .github/workflows/prebuild.sh
@@ -11,7 +11,7 @@ cd build
 
 # We install STARE to /usr/local/include/STARE/ and /usr/local/lib/ .. seems easist.
 #cmake -DSTARE_INSTALL_LIBDIR=lib -DBUILD_SHARED_LIBS=NO ..
-cmake -DCMAKE_INSTALL_PREFIX=/home/runner/stare -DSTARE_INSTALL_LIBDIR=lib -DBUILD_SHARED_LIBS=NO ..
+cmake -DCMAKE_INSTALL_PREFIX=~/stare -DSTARE_INSTALL_LIBDIR=lib -DBUILD_SHARED_LIBS=NO ..
 make -j4
 make install
 
@@ -22,8 +22,8 @@ cd
 # https://github.com/multi-build/multibuild/issues/470
 git config --global --add safe.directory "*"
 
-export STARE_LIB_DIR=/home/runner/stare/lib/
-export STARE_INCLUDE_DIR=/home/runner/stare/include/STARE/
+export STARE_LIB_DIR=~/stare/lib/
+export STARE_INCLUDE_DIR=~/stare/include/STARE/
         
         
         
